261d511b4d38e39bb405e7e6032483a1d9253d4c
sina <sina.backend@gmail.com>
2025-08-02 11:24:58 +0430

    names urls

--- django_traning/store/store/urls.py
+++ django_traning/store/store/urls.py
@@ -2,5 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("hello/",views.say_hello)
+    path("hello/",views.say_hello) , 
+    path("name/",views.say_name)
+    
 ]
--- django_traning/store/store/views.py
+++ django_traning/store/store/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def say_hello (request):
     return HttpResponse("hello")
+def say_name(request):
+    user_name = input("pleas enter your name :   ")
+    return HttpResponse(f"hello {user_name}")
